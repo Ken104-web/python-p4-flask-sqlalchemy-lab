91c38ecc1ff0cfb349b92408b59c47c8c2eb48e5
KenCorp <kennethwarui35@gmail.com>
2023-09-19 12:43:43 +0300

    Completed lab

--- server/app.py
+++ server/app.py
@@ -19,11 +19,37 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    if animal:
+        body = f'''
+            <ul>ID: {animal.id}</ul>
+            <ul>Name: {animal.name}</ul>
+            <ul>Species: {animal.species}</ul>
+            <ul>Zookeeper: {animal.zookeeper.name}</ul>
+            <ul>Enclosure: {animal.enclosure.environment}</ul>
+        '''
+    else:
+        body = '<h1>404 animal not found</h1>'
+    responce = make_response(body)
+    return responce
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper= Zookeeper.query.filter(Zookeeper.id == id).first()
+    if zookeeper:
+        body = f'''
+        <ul>ID: {zookeeper.id}</ul>
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>'''
+    for animal in zookeeper.animals:
+            body += f'<ul>Animal: {animal.name}</ul>'
+        
+            body += '</ul></ul>'
+    else:
+        body = '<h1>404 zookeeper not found</h1>'
+    
+    responce = make_response(body)
+    return responce
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
